f1603455c31af9e71f2e3c321152a3eb1668c555
stefa <Imperivm94>
2019-07-17 19:05:13 +0200

    Aggiunto codice per la pressione dei pulsanti

--- app/src/main/java/simple/cipher/MainActivity.java
+++ app/src/main/java/simple/cipher/MainActivity.java
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +19,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button caesar = findViewById(R.id.caesar);
-        Button mono = findViewById(R.id.mono);
+        Button caesar = (Button) findViewById(R.id.caesar);
+        Button mono = (Button) findViewById(R.id.mono);
+        final Alphabet a = new Alphabet();
+        final Cipher c = new Cipher(a);
+
+        mono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText plain = (EditText) findViewById(R.id.cifrare);
+                TextView crypted = (TextView) findViewById(R.id.cifrato);
+
+                List<Character> alpha = new ArrayList<>(a.alpha);
+                Collections.shuffle(alpha);
+                String s = c.monoCipher(plain.getText().toString(),alpha);
+
+                crypted.setText(s);
+
+            }
+        });
 
         caesar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                EditText plain = (EditText) findViewById(R.id.cifrare);
+                TextView crypted = (TextView) findViewById(R.id.cifrato);
+
+                String s = c.caesarCipher(plain.getText().toString(),5);
+
+                crypted.setText(s);
             }
         });
+
+
 
     }
 }
